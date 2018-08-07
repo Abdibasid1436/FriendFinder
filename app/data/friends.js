573471b2234ed8697b989eedd6145024b6fd8409
Abdibasid Osman <mistuyeta@me.com>
2018-08-07 18:15:11 -0500

    comment

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var friendMatch = [
+    {
+      name: "Kirk Cousins",
+      photo: "https://i.kinja-img.com/gawker-media/image/upload/s--16dwrE25--/c_scale,f_auto,fl_progressive,q_80,w_800/cbb7wo8dgo3sygugdl7k.jpg ",
+      scores: ["2", "2", "2", "2", "2"]
+    }, {
+      name: "Eden Hazard",
+      photo: "http://www.theboltonnews.co.uk/resources/images/8161407.jpg?display=1&htype=0&type=responsive-gallery ",
+      scores: ["3", "3", "3", "3", "3"]
+    }, {
+      name: " Lebron James",
+      photo: "https://cdn.vox-cdn.com/thumbor/hn5OcjPBR0KFeEowZ1l4855PB3E=/0x0:2110x1407/1820x1213/filters:focal(709x164:1045x500):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/60349477/lebron_ssr_swap.0.jpg ",
+      scores: ["1", "1", "1", "1", "1"]
+    }, {
+      name: "Messi",
+      photo: "https://www.thesun.co.uk/wp-content/uploads/2017/05/nintchdbpict0003195822611-e1494600615564.jpg?strip=all&w=787 ",
+      scores: ["3", "3", "3", "3", "1"]
+    },
+  
+  ];
+  
+  module.exports = friendMatch;
+  
